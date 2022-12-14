76078e1e0abc3f0116d421cf5daf060774afabf4
AKJ <kjajila@gmail.com>
2022-12-14 16:19:58 -0500

    remove provider from globalState

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -2,13 +2,5 @@ import React, {useContext } from "react";
 import { useProductReducer } from './reducers'
 import { createStore } from 'redux';
 
+export default createStore(useProductReducer);
 
-let store = createStore(useProductReducer);
-const StoreProvider = ({ value = [], ...props }) => {
-  const [state, dispatch] = useProductReducer();
-
-  return <Provider value={[state, dispatch]} {...props} />;
-};
-
-
-export { StoreProvider };
